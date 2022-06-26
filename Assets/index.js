34b790f8094368d2a81e3d7dc1cf43f32d2114d6
Eddy Cruz <eduardoangelcruz1997@gmail.com>
2022-06-25 20:33:14 -0700

    homework-1-commit

---
+++ Assets/index.js
@@ -0,0 +1,7 @@
+
+
+function togglemenu(){
+    var menuList = document.getElementsByClassName("menuList")[0]
+    var navmenu = document.getElementsByClassName("navmenu")[0]
+    navmenu.classList.toggle('shown')
+}
